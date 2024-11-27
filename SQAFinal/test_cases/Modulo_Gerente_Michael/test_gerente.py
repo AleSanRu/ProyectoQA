befc45d02ac233acbb6e7a1213a305d313f37090
xXMamboxXx <prm2024434@est.univalle.edu>
2024-11-27 13:48:00 -0400

    testeo

--- SQAFinal/test_cases/Modulo_Gerente_Michael/test_gerente.py
+++ SQAFinal/test_cases/Modulo_Gerente_Michael/test_gerente.py
@@ -24,9 +24,9 @@ class TestAtt:
 
         #inicio
         self.driver.find_element(By.XPATH,"//span[@class = 'mdi mdi-bell-outline text-2xl text-gray-500 cursor-pointer']").click()
-        time.sleep(4)
+        time.sleep(7)
         self.driver.find_element(By.XPATH,"//span[text() = 'Inicio']").click()
-        time.sleep(4)
+        time.sleep(5)
         self.driver.find_element(By.XPATH,"//span[@class= 'mdi mdi-fullscreen text-2xl text-gray-500']").click()
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//span[@class= 'mdi mdi-fullscreen text-2xl text-gray-500']").click()
@@ -36,7 +36,7 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//span[@class = 'text-md tracking-tight']").click()
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//a[@class = 'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white']").click()
-        time.sleep(4)
+        time.sleep(5)
         self.driver.find_element(By.XPATH,"//button[contains(@class, 'text-white bg-gradient-to-br from-green-400')]").click()
         time.sleep(2)
         self.driver.find_element(By.XPATH,"//button[contains(@class, 'text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600')]").click()
@@ -64,7 +64,7 @@ class TestAtt:
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//option[@value = 'Gerente']").click()
         time.sleep(2)
-        self.driver.find_element(By.XPATH,"//div[@class = 'p-2 sm:ml-64']").click()
+        self.driver.find_element(By.XPATH,"//div[@class = 'hidden sm:ml-10 sm:flex p-5']").click()
         time.sleep(2)
         self.driver.find_element(By.XPATH,"(//input[@type = 'checkbox']//parent::div)[1]").click()
         time.sleep(0.1)
@@ -100,11 +100,10 @@ class TestAtt:
         time.sleep(5)
 
         #Estudaintes
-        self.driver.find_element(By.XPATH,"(//span[@class = 'ms-3 text-xs']//parent::span)[4]").click()
+        self.driver.find_element(By.XPATH,"//span[text()= 'Estudiantes']").click()
         time.sleep(9)
 
         #CREAR
-
         self.driver.find_element(By.XPATH,"//div[@class = 'flex justify-between items-center mb-4']//button[contains(@class, 'inline-flex items-center justify-center')]").click()
         time.sleep(2)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").send_keys('Michael Bryan')
@@ -136,11 +135,11 @@ class TestAtt:
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[contains(@class, 'text-white bg-gradient-to-r from-green-400')]").click()
-        time.sleep(8)
+        time.sleep(9)
 
         #VER PERFIL
         self.driver.find_element(By.XPATH,"//input[@type = 'search']").send_keys('6894308')
-        time.sleep(8)
+        time.sleep(10)
         self.driver.find_element(By.XPATH,"//div[contains(@class, 'relative w-11 h-6 bg-gray-200 peer-focus:outline-none')]").click()
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[text() = 'Confirmar']").click()
@@ -150,13 +149,13 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//button[text() = 'Confirmar']").click()
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[text() = 'Ver Perfil']").click()
-        time.sleep(8)
+        time.sleep(10)
         self.driver.find_element(By.XPATH,"//button[text() = 'Volver']").click()
         time.sleep(7)
 
         #Editar
         self.driver.find_element(By.XPATH,"//input[@type = 'search']").send_keys('6894308')
-        time.sleep(8)
+        time.sleep(10)
         self.driver.find_element(By.XPATH,"//button[text() = 'Editar']").click()
         time.sleep(5)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").clear()
@@ -204,7 +203,7 @@ class TestAtt:
 
         #ELIMINAR
         self.driver.find_element(By.XPATH,"//input[@type = 'search']").send_keys('2033166')
-        time.sleep(8)
+        time.sleep(10)
         self.driver.find_element(By.XPATH,"//button[text() = 'Eliminar']").click()
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[text() = 'Confirmar']").click()
@@ -250,7 +249,7 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//input[@type = 'search']").send_keys('6894308')
         time.sleep(8)
         self.driver.find_element(By.XPATH,"//button[text() = 'Editar']").click()
-        time.sleep(4)
+        time.sleep(7)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").clear()
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").send_keys('Miguel Perez')
@@ -290,7 +289,7 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//input[@name = 'specialty']").send_keys('Diseñador')
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//button[text() = 'Actualizar']").click()
-        time.sleep(5)
+        time.sleep(7)
 
         #ELIMINAR
 
@@ -299,7 +298,7 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//button[text() = 'Eliminar']").click()
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[text() = 'Confirmar']").click()
-        time.sleep(5)
+        time.sleep(6)
 
         #GESTION DE CURSOS
         #MODALIDADES
@@ -317,16 +316,16 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//input[@name = 'duration_in_months']").send_keys('12')
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//button[text() = 'Guardar']").click()
-        time.sleep(6)
+        time.sleep(12)
 
         #EDITAR
 
         self.driver.find_element(By.XPATH,"//input[@type = 'search']").send_keys('Diseño Grafico')
-        time.sleep(10)
+        time.sleep(13)
         self.driver.find_element(By.XPATH,"//button[text() = 'Editar']").click()
-        time.sleep(4)
+        time.sleep(5)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").clear()
-        time.sleep(3)
+        time.sleep(4)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").send_keys('Renderizado Grafico')
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//input[@name = 'duration_in_months']").clear()
@@ -381,33 +380,33 @@ class TestAtt:
         self.driver.find_element(By.XPATH,"//input[@name = 'cost']").send_keys('1000')
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//button[text() = 'Guardar']").click()
-        time.sleep(9)
+        time.sleep(10)
 
         #Editar
         self.driver.find_element(By.XPATH,"(//button[text() = 'Ver Detalles']//parent::button)[4]").click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH,"//button[text() = 'Editar']").click()
-        time.sleep(6)
-        self.driver.find_element(By.XPATH,"//input[@name = 'name']").clear()
         time.sleep(3)
+        self.driver.find_element(By.XPATH,"//button[text() = 'Editar']").click()
+        time.sleep(7)
+        self.driver.find_element(By.XPATH,"//input[@name = 'name']").clear()
+        time.sleep(4)
         self.driver.find_element(By.XPATH,"//input[@name = 'name']").send_keys('Renderizad lo que sea')
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//select[@name = 'parallel']").click()
         time.sleep(1)
         self.driver.find_element(By.XPATH,"//select[@name = 'parallel']//option[@value = 'B']").click()
         time.sleep(1)
+        self.driver.find_element(By.XPATH,"//h2[text() = 'Actualizar datos del Curso']").click()
+        time.sleep(3)
         self.driver.find_element(By.XPATH,"//input[@name = 'description']").clear()
-        time.sleep(1)
+        time.sleep(3)
         self.driver.find_element(By.XPATH,"//input[@name = 'description']").send_keys('aqui hay clases equisde asd asd')
-        time.sleep(1)
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
+        time.sleep(2)
         self.driver.find_element(By.XPATH,"//input[@name = 'cost']").clear()
-        time.sleep(3)
+        time.sleep(4)
         self.driver.find_element(By.XPATH,"//input[@name = 'cost']").send_keys('1500')
         time.sleep(3)
         self.driver.find_element(By.XPATH,"//button[text() = 'Actualizar']").click()
-        time.sleep(5)
+        time.sleep(7)
 
         #Eliminar
         self.driver.find_element(By.XPATH,"(//button[text() = 'Ver Detalles']//parent::button)[4]").click()
